e2095bcc80d9fa99d90c52b85857fc254d80d292
Elton Zheng <elton.zheng.sh@gmail.com>
2021-03-17 19:42:59 +0800

    Feat: add next-auth and add sign in function.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import Head from 'next/head';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export default function Home() {
+  const [session, loading] = useSession();
   return (
     <>
-      <div className='w-2/3 mx-auto'>Hello auth-demo</div>
+      <Head>
+        <title>Auth Demo</title>
+      </Head>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {session.user.name}
+          <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
     </>
   );
 }
