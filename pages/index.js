68f56314af4b4a496f0b5add93785c0c674cea17
Elton Zheng <elton.zheng.sh@gmail.com>
2021-03-21 12:51:33 +0800

    add dark mode support.

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,16 @@ export default function Home() {
   return (
     <Layout>
       <h1 className='text-3xl mb-3 font-semibold dark:text-gray-200'>
-        NextAUth.js Example
+        NextAuth.js Example
       </h1>
       <p className='text-gray-700 dark:text-gray-300'>
         This is an example site to demonstrate how to use{' '}
-        <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
+        <a
+          href={`https://next-auth.js.org`}
+          className='text-lightBlue-600 underline'>
+          NextAuth.js
+        </a>{' '}
+        for authentication.
       </p>
     </Layout>
   );
